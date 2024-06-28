687361462c337235d1c1c3eaa976880f8dda37da
Navdeep <navdeepsingh92@gmail.com>
2024-06-28 10:53:02 +0530

    full code

---
+++ app/(root)/layout.jsx
@@ -0,0 +1,39 @@
+import { Inter } from "next/font/google";
+import "bootstrap/dist/css/bootstrap.css";
+
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import "../index.css";
+import Header from "@/components/Common/Header";
+import Footer from "@/components/Common/Footer";
+import "bootstrap/dist/css/bootstrap.css";
+
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import "../../styles/custom.css";
+import Head from "next/head";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Fluentie",
+  description: "Educational website",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Cormorant:ital,wght@0,300..700;1,300..700&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body className={inter.className}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
